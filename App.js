9550e32feeac7f2f23b0558af75f8557949dcca0
Анатолий Кучин <akuchin@svyaznoy.ru>
2019-10-21 15:31:40 +0300

    v2

--- App.js
+++ App.js
@@ -1,92 +1,21 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SectionList } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import styled from 'styled-components/native';
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import { Appointment, SectionTitle } from './components';
+import { HomeScreen, PatientScreen } from './screens';
 
-const DATA = [
+const AppNavigator = createStackNavigator(
   {
-    title: '17 октября',
-    data: [
-      {
-        time: '15:30',
-        active: true,
-        diagnosis: 'пульпит',
-        user: {
-          fullname: 'Максим Максаков',
-          avatar: 'https://sun9-70.userapi.com/c852124/v852124111/b7b76/jdS_fJoAi-s.jpg'
-        }
-      },
-      {
-        time: '18:00',
-        diagnosis: 'удаление зуба',
-        user: {
-          fullname: 'Алексей Калюжный',
-          avatar: 'https://sun9-55.userapi.com/c852224/v852224328/67b60/H_wuyzJeZr0.jpg'
-        }
-      }
-    ]
+    Home: {
+      screen: HomeScreen
+    },
+    Patient: {
+      screen: PatientScreen
+    }
   },
   {
-    title: '18 октября',
-    data: [
-      {
-        time: '11:00',
-        diagnosis: 'пульпит',
-        user: {
-          fullname: 'Анатолий Кучин',
-          avatar: 'https://sun9-16.userapi.com/c824411/v824411735/13de79/AEpd71COOKM.jpg?ava=1'
-        }
-      },
-      {
-        time: '13:20',
-        diagnosis: 'удаление зуба',
-        user: {
-          fullname: 'Павел Дуров',
-          avatar: 'https://sun9-16.userapi.com/c824411/v824411735/13de79/AEpd71COOKM.jpg?ava=1'
-        }
-      }
-    ]
+    initialRouteName: 'Home'
   }
-]
+);
 
-export default function App() {
-  return (
-    <Container>
-      <SectionList
-        sections={DATA}
-        keyExtractor={(item, index) => index}
-        renderItem={({ item }) => <Appointment {...item} /> }
-        renderSectionHeader={({ section: { title }}) => (
-          <SectionTitle>{ title }</SectionTitle>
-        )}
-      />
-      <PlusButton>
-        <Ionicons name='ios-add' size={36} color='white' />
-      </PlusButton>
-    </Container>
-  );
-}
-
-const PlusButton = styled.TouchableOpacity`
-  align-items: center;
-  justify-content: center;
-  border-radius: 50px;
-  width: 64px;
-  height: 64px;
-  background: #2A86FF;
-  position: absolute;
-  right: 25px;
-  bottom: 25px;
-  shadow-color: #2a86ff;
-  shadow-opacity: 0.7;
-  shadow-radius: 3.5;
-  elevation: 4;
-`;
-
-const Container = styled.View`
-  flex: 1;
-  margin-top: 30px;
-  padding: 0 20px;
-`;
+export default createAppContainer(AppNavigator);
